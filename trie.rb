ce97464b5a70cf139b9e14aab0cd8d3b79eab544
Alex Guo <alexqguo@gmail.com>
2014-03-19 16:52:42 -0400

    trie can print all words

--- trie.rb
+++ trie.rb
@@ -1,3 +1,5 @@
+require_relative 'trie_node'
+
 class Trie
   
   attr_reader :root
@@ -15,54 +17,22 @@ class Trie
     end
   end
   
+  def all_words
+    @root.check_for_words("")
+  end
+  
 end
 
 
-
-class TrieNode
-  
-  attr_reader :value, :parent, :children
-  attr_accessor :end_of_word
-  
-  def initialize(value, parent, end_of_word = false, children = [])
-    raise "Invalid TrieNode Value - #{value}" unless value.is_a?(String) && value.length <= 1
-    @value, @parent, @end_of_word, @children = value, parent, end_of_word, children
-  end
-  
-  def has_child?(char)
-    @children.include?(char)
-  end
-  
-  def add_child(char)
-    unless has_child?(char)
-      new_child = TrieNode.new(char, self)
-      @children << new_child
-    
-      new_child
-    else
-      nil
-    end
-  end
-  
-  def find_child(char)
-    @children.find { |child| child.value == char }
-  end
-  
-  def add_or_find_child(char)
-    find_child(char) || add_child(char)
-  end
-  
-  def children_values
-    @children.map(&:value)
-  end
-end
 
 =begin
 
+load 'trie.rb'
 node = TrieNode.new("a", nil)
 node.add_child("d")
 node.add_child("c")
 
+load 'trie.rb'
 trie = Trie.new
 trie.add_word("help")
 trie.add_word("hell")
---
+++ trie_node.rb
@@ -0,0 +1,46 @@
+class TrieNode
+  
+  attr_reader :value, :parent, :children
+  attr_accessor :end_of_word
+  
+  def initialize(value, parent, end_of_word = false, children = [])
+    raise "Invalid TrieNode Value - #{value}" unless value.is_a?(String) && value.length <= 1
+    @value, @parent, @end_of_word, @children = value, parent, end_of_word, children
+  end
+  
+  def has_child?(char)
+    @children.include?(char)
+  end
+  
+  def add_child(char)
+    unless has_child?(char)
+      new_child = TrieNode.new(char, self)
+      @children << new_child
+    
+      new_child
+    else
+      nil
+    end
+  end
+  
+  def find_child(char)
+    @children.find { |child| child.value == char }
+  end
+  
+  def add_or_find_child(char)
+    find_child(char) || add_child(char)
+  end
+  
+  def children_values
+    @children.map(&:value)
+  end
+  
+  def check_for_words(prefix)
+    puts prefix + @value if @end_of_word
+    
+    prefix += @value
+    @children.each { |child| child.check_for_words(prefix) }
+  end
+
+  
+end
